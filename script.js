efd5bb4d7b5de532ffd0fbef70782b81f7360f9a
praelia <FirstRankGames@gmail.com>
2024-12-17 08:44:55 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+canvas.height = window.innerHeight;
+canvas.width = window.innerHeight*4/3;
+var playerx = 0;
+var playery = 0;
+var tileset = document.getElementById("tileset");
+var tileSize = 32;
+
+function printTile(xtile, ytile, printx, printy) {
+  ctx.drawImage(tileset,xtile*tileSize,ytile*tileSize,tileSize,tileSize,printx*canvas.width/12,printy*canvas.height/9,canvas.width/12,canvas.height/9);
+}
+
+printTile(0,0,0,0);
