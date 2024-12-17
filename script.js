faf3d65909c7db471a69a94a91a90e4cd04d6150
praelia <FirstRankGames@gmail.com>
2024-12-17 09:11:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('myCanvas');
+var canvas = document.getElementById('myCancanvasvas');
 var ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerHeight*4/3;
@@ -8,8 +8,6 @@ var tileset = document.getElementById("tileset");
 var tileSize = 32;
 
 function printTile(xtile, ytile, printx, printy) {
-  var canvas = document.getElementById('myCanvas');
-  var ctx = canvas.getContext('2d');
   ctx.drawImage(tileset,xtile*tileSize,ytile*tileSize,tileSize,tileSize,printx*canvas.width/12,printy*canvas.height/9,canvas.width/12,canvas.height/9);
 }
 
