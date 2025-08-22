7959ee044b616b6271ed42935638279f34146a01
praelia <FirstRankGames@gmail.com>
2025-08-22 13:02:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ var map = "\
 GqGwgxgcGs\
 GaGsGsGsGs\
 GzGxGxgwge\
-dqdwdwdeGz";
-var mapHeight = 4;
+dqdwdwdeGz\
+dzdxdxdxdc\
+EqEwEwEwEe\
+EaEsEsEsEd";
+var mapHeight = 7;
 var mapWidth = 5;
 
 function printTile(xtile, ytile, printx, printy) {
@@ -27,12 +30,12 @@ function getTile(tile, j, i) {
     'Sq': [0, 3], 'Sw': [1, 3], 'Se': [2, 3], 'sq': [3, 3], 'sw': [4, 3], 'se': [5, 3], 'Dq': [6, 3], 'Dw': [7, 3], 'De': [8, 3], 'dq': [9, 3], 'dw': [10, 3], 'de': [11, 3],
     'Sa': [0, 4], 'Ss': [1, 4], 'Sd': [2, 4], 'sa': [3, 4], 'ss': [4, 4], 'sd': [5, 4], 'Da': [6, 4], 'Ds': [7, 4], 'Dd': [8, 4], 'da': [9, 4], 'ds': [10, 4], 'dd': [11, 4],
     'Sz': [0, 5], 'Sx': [1, 5], 'Sc': [2, 5], 'sz': [3, 5], 'sx': [4, 5], 'sc': [5, 5], 'Dz': [6, 5], 'Dx': [7, 5], 'Dc': [8, 5], 'dz': [9, 5], 'dx': [10, 5], 'dc': [11, 5], 
-    'Eq': [aT, 6], 'Ew': [aT+1, 6], 'Ee': [aT+2, 6],
-    'Ea': [aT, 7], 'Es': [aT+1, 7], 'Ed': [aT+2, 7],
-    'Ez': [aT, 8], 'Ex': [aT+1, 8], 'Ec': [aT+2, 8],
-    'Bq': [aT, 9], 'Bw': [aT+1, 9], 'Be': [aT+2, 9],
-    'Ba': [aT,10], 'Bs': [aT+1,10], 'Bd': [aT+2,10],
-    'Bz': [aT,11], 'Bx': [aT+1,11], 'Bc': [aT+2,11]
+    'Eq': [aT*3, 6], 'Ew': [aT*3+1, 6], 'Ee': [aT*3+2, 6],
+    'Ea': [aT*3, 7], 'Es': [aT*3+1, 7], 'Ed': [aT*3+2, 7],
+    'Ez': [aT*3, 8], 'Ex': [aT*3+1, 8], 'Ec': [aT*3+2, 8],
+    'Bq': [aT*3, 9], 'Bw': [aT*3+1, 9], 'Be': [aT*3+2, 9],
+    'Ba': [aT*3,10], 'Bs': [aT*3+1,10], 'Bd': [aT*3+2,10],
+    'Bz': [aT*3,11], 'Bx': [aT*3+1,11], 'Bc': [aT*3+2,11]
   };
   if (tile in tileMap) {
     const [x, y] = tileMap[tile];
@@ -46,11 +49,12 @@ function printMap() {
       getTile(tile, j, i, tileset);
     }
   }
+  if (aT == 3) {aT = 0;} else {aT = aT + 1;}
 }
 
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 tileset.onload = function() {
-  printMap();
+  setInterval(printMap, 200);
 };
